e1585d751fd234f07112aa7d831e61134559481d
Tiago F. Hintehrolz <fh.tiago@gmail.com>
2025-01-20 16:58:10 -0300

    Configuração do arquivo config, init da api e primeiro model implementado.

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
 DEBUG = True
+
+USERNAME = 'tiago'
+PASSWORD = 'tiago'
+SERVER = 'localhost'
+DB = 'api_flask'
+
+SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
