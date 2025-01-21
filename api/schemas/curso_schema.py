11f289577c99b366853755b7070e3a08813a07ce
Tiago F. Hintehrolz <fh.tiago@gmail.com>
2025-01-21 14:07:03 -0300

    Arquivo CursoSchema, criação da classe Meta para atributos.

--- api/schemas/curso_schema.py
+++ api/schemas/curso_schema.py
@@ -3,9 +3,10 @@ from ..models import curso_model
 from marshmallow import fields
 
 class CursoSchema(ma.SQLAlchemyAutoSchema):
-    model = curso_model.Curso
-    load_instance = True
-    fields = ('id', 'nome', 'descricao', 'data_publicacao')
+    class Meta:
+        model = curso_model.Curso
+        load_instance = True
+        fields = ('id', 'nome', 'descricao', 'data_publicacao')
     
     nome = fields.String(required=True)
     descricao = fields.String(required=True)
